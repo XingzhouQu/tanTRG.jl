fbc1104f1eb01adcb6a418fbf0fba53deae4fbf0
Junsen Wang <jsw@mail.ustc.edu.cn>
2023-02-21 00:23:43 +0800

    *

--- src/rhoMPO.jl
+++ src/rhoMPO.jl
@@ -4,8 +4,30 @@ TODO:
 implement two-site variational product
 
 """
+function getFe(trRho, beta, lgtr0)
+    return -1*(beta)^-1*(log(trRho)+lgtr0)
+end
+
 function rhoMPO(H::MPO, beta0::Number, s; tol=1e-6)
-    rho = MPO(s, "Id")
+    nmaxHn = 8
+    lstrHn = Vector{Float64}(undef, nmaxHn)
+
+    Hid = MPO(s, "Id")
+    tr0 = tr(Hid)
+    Hid /= tr0
+    trHid = tr(Hid)
+
+    Hn = copy(H)
+    Hn /= tr0
+    lstrHn[1] = tr(Hn)
+
+    for i in 2:nmaxHn
+        Hn = apply(H, Hn)
+        lstrHn[i] = tr(Hn)
+    end
+
+
+
     lgnrm = lognorm(rho)
     nrm0 = exp(lgnrm)
     rho /= nrm0
